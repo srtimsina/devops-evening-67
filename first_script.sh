e7441313503736d7c4c8d85852c238df637c2e26
Suryaraj Timsina <devopsuryaraj@gmail.com>
2026-03-31 13:17:34 +0000

    added logout page

--- first_script.sh
+++ first_script.sh
@@ -2,10 +2,17 @@
 
 sudo apt update
 sudo apt install apache2 -y
-sudo systemctl start apache2
 sudo systemctl status apache2
 
 
 echo "Hello DevOps" > /var/www/html/index.html
 
 echo "Some random change"
+
+sudo systemctl restart apache2
+
+df -h
+
+free -m
+
+useradd -d -m /home/user1 user1
